658ae40eb1d0e96bb8b69b0aada0e0248f9d496f
RemusCourtenay <remuscourtenay@gmail.com>
2025-11-16 18:03:44 +1300

    Finished Harriers section

---
+++ RemusSeleniumProject/Models/WebscorerRace.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using OpenQA.Selenium;
+
+namespace RemusSeleniumProject.Models;
+
+public record WebscorerRace
+{
+    public int RowIndex { get; set; }
+    
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+
+    public string FileName => Name.Replace("/", "_").Replace("\"", "_") + " Edit.txt";
+    public string AlternateFileName => Name.Replace("/", "_").Replace("\"", "_") + "  Edit.txt";
+    
+    public DateTime Date { get; set; }
+    
+    public Uri Uri { get; set; }
+
+    public WebscorerRace(IWebDriver driver, IWebElement element)
+    {
+        RowIndex = int.Parse(element.GetAttribute("id")!.Split('_').Last());
+        
+        var href = element.GetAttribute("href");
+
+        Id = int.Parse(href!.Split('=').Last());
+        Uri = new Uri(href);
+
+        Name = element.Text;
+
+        var dateElement = driver.FindElement(By.Id($"CPH1_repMyPostedRaces_lbRaceDate_{RowIndex}"));
+        
+        Date = DateTime.ParseExact(dateElement.Text, "MMM d, yyyy", CultureInfo.InvariantCulture);
+    }
+
+    public bool IsInList(IEnumerable<string> raceNames)
+    {
+        return raceNames.Any(rn => Name.Contains(rn));
+    }
+}
--- RemusSeleniumProject/Program.cs
+++ RemusSeleniumProject/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace RemusSeleniumProject;
+﻿using OpenQA.Selenium.Support.UI;
+using RemusSeleniumProject.Models;
+
+namespace RemusSeleniumProject;
 
 using System;
 using OpenQA.Selenium;
@@ -6,25 +9,76 @@ using OpenQA.Selenium.Chrome;
 
 public static class FirstScript
 {
-    public static void Main()
+    public const string ResultsURL = "https://www.webscorer.com/WH_Results";
+
+    public static readonly string[] IgnoreRaceNames = ["Milnes Beatson Motueka", "2 Person Relay"];
+
+    public static DateTime StartDateTime = new(2023, 11, 1);
+    
+    public static async Task Main()
     {
         IWebDriver driver = new ChromeDriver();
 
-        driver.Navigate().GoToUrl("https://www.selenium.dev/selenium/web/web-form.html");
-
-        var title = driver.Title;
-
+        driver.Navigate().GoToUrl(ResultsURL);
+        
         driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
 
-        var textBox = driver.FindElement(By.Name("my-text"));
-        var submitButton = driver.FindElement(By.TagName("button"));
-            
-        textBox.SendKeys("Selenium");
-        submitButton.Click();
-            
-        var message = driver.FindElement(By.Id("message"));
-        var value = message.Text;
+        var raceElements = driver.FindElements(By.CssSelector("a[id*='CPH1_repMyPostedRaces_racetxtlink']"));
+        
+        var races = raceElements.Select(re => new WebscorerRace(driver, re)).ToList();
+
+        races = races
+            .Where(r => r.Date >= StartDateTime)
+            .Where(r => !r.IsInList(IgnoreRaceNames))
+            .ToList();
+
+        foreach (var race in races)
+        {
+            Console.WriteLine(race);
+            await DownloadRaceFile(driver, race);
+        }
             
         driver.Quit();
+    }
+
+    public static async Task DownloadRaceFile(IWebDriver driver, WebscorerRace race)
+    {
+        Console.WriteLine($"Downloading race file for race: '{race.RowIndex}'");
+        
+        await driver.Navigate().GoToUrlAsync(race.Uri);
+
+        // Open dropdown
+        driver.FindElement(By.Id("btnDownloadEdit")).Click();
+        
+        var downloadElement = driver.FindElement(By.Id("CPH1_RaceLinksButtons1_btnDownloadEditTxt"));
+        
+        WebDriverWait waitForElement = new WebDriverWait(driver, TimeSpan.FromSeconds(1));
+        waitForElement.Until(_ => downloadElement.Displayed);
+        
+        downloadElement.Click();
+        
+        var downloadPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+        
+        var fileName = Path.Combine(downloadPath, race.FileName);
+        var alternateFilePath = Path.Combine(downloadPath, race.AlternateFileName);
+        
+        var copyPath = Path.Combine("C:\\Users\\Remus\\RiderProjects\\RemusSeleniumProject\\RemusSeleniumProject\\Downloaded", race.FileName);
+        
+        
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+        wait.Until(d => File.Exists(fileName) ||  File.Exists(alternateFilePath));
+
+        try
+        {
+            File.Move(fileName, copyPath, true);
+        }
+        catch (FileNotFoundException e)
+        {
+            File.Move(alternateFilePath, copyPath, true);
+        }
+        
+
+        await driver.Navigate().BackAsync();
     }
 }
